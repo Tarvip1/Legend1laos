b414be6482099c7700d31539f322da27d9d30939
Tarvip1 <saruregency@gmail.com>
2025-09-06 02:06:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-{
+const urls = {
   "1": "http://legend147-reedix.store",
   "2": "http://legend147-Lilyvia.store",
   "3": "http://legend147-bamboora.store"
+};
+
+export default async function handler(req, res) {
+  res.status(200).json(urls);
 }
